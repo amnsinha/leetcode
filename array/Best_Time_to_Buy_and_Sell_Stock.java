e9c2a7823ca8ca6b46549851f719b12743899cc6
amnsinha <sinha.amn@gmail.com>
2022-04-24 22:43:06 +0530

    Best_Time_to_Buy_and_Sell_Stock

--- array/Best_Time_to_Buy_and_Sell_Stock.java
+++ array/Best_Time_to_Buy_and_Sell_Stock.java
@@ -4,6 +4,26 @@ public class Best_Time_to_Buy_and_Sell_Stock {
 
     public static void main(String[] args) {
 
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+
+
+    }
+
+
+    public static int maxProfit(int[] prices) {
+        int largestprofit = 0;
+        int minSofar = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+
+            if(prices[i] < minSofar){
+                minSofar = prices[i];
+            }
+            else {
+                largestprofit = Math.max(largestprofit, prices[i]-minSofar);
+            }
+
+
+        } return largestprofit;
     }
 
 }
